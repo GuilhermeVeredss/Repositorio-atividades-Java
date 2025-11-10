2e94d0b6de361313d5b84152385ecea5e2fa6452
GuilhermeVeredas <guilherme.silva@veredaseducacao.com.br>
2025-11-10 14:22:41 -0300

    Modificações da classe Gato e sua Main

--- Estatico/Estatico atividade/src/Gato.java
+++ Estatico/Estatico atividade/src/Gato.java
@@ -1,6 +1,21 @@
 public class Gato {
+    public static int quantidade_de_gatos=0; 
     public String nome;
     public int idade;
 
+    public Gato(String nom, int idad) {
+        nome = nom;
+        idade = idad;
+
+        quantidade_de_gatos++;
+    }
+
+    public static void verNumeroGatos(){
+        System.out.println("Quantidade de gatos: " + quantidade_de_gatos);
+    }
+
+    public void ver(){
+        System.out.println("Nome: " + nome + "\nIdade: " + idade);
+    }
 
 }
--- Estatico/Estatico atividade/src/Main.java
+++ Estatico/Estatico atividade/src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+
+        Gato g1 = new Gato("Garfield", 4);
+        Gato g2 = new Gato("Gato de Botas", 2);
+        Gato g3 = new Gato("Tom", 8);
+
+
+        g1.ver();
+        System.out.println("\n");
+
+        g2.ver();
+        System.out.println("\n");
+
+        g3.ver();
+        System.out.println("\n");
+
+        Gato.verNumeroGatos();
+
+        System.out.println("Quantidade de gatos: " + Gato.quantidade_de_gatos);
+
+        Double media = calcularMedia(4.0, 8.0);
+
+        System.out.println("Valor da média: " + media);
+
+    }
+
+    public static Double calcularMedia(Double a, Double b){
+
+        return (a+b)/2;
     }
 }
